8dc33e9c23d76b7e48a1b0e324ec6047d1440404
Karrick S. McDermott <kmcdermott@linkedin.com>
2015-09-29 00:56:42 -0700

    includes ErrList type

---
+++ errors.go
@@ -0,0 +1,45 @@
+package gorill
+
+import "strings"
+
+// ErrList is a slice of errors, useful when a function must return a single error, but has multiple
+// independent errors to return.
+type ErrList []error
+
+// Error returns the string version of an error list, which is the list of errors, joined by a
+// comma-space byte sequence.
+func (e ErrList) Error() string {
+	es := make([]string, len([]error(e)))
+	for i := range []error(e) {
+		if []error(e)[i] != nil {
+			es[i] = []error(e)[i].Error()
+		}
+	}
+	return strings.Join(es, ", ")
+}
+
+// Append appends non-nil errors to the list of errors.
+func (e *ErrList) Append(b error) {
+	if b != nil {
+		*e = append(*e, b)
+	}
+}
+
+// Err returns either a list of non-nil error values, or a single error value if the list only
+// contains one error.
+func (e ErrList) Err() error {
+	errors := make([]error, len([]error(e)))
+	for _, e := range []error(e) {
+		if e != nil {
+			errors = append(errors, e)
+		}
+	}
+	switch len(errors) {
+	case 0:
+		return nil
+	case 1:
+		return e[0]
+	default:
+		return ErrList(errors)
+	}
+}
