fb002d250f6337653472da77ef0e4a33967d7412
Karrick S. McDermott <karrick@karrick.net>
2019-04-05 11:07:02 -0400

    ensure functions can be used for both tests and benchmarks

--- ensure_test.go
+++ ensure_test.go
@@ -6,39 +6,39 @@ import (
 	"testing"
 )
 
-func ensureBuffer(t *testing.T, buf []byte, n int, want string) {
-	t.Helper()
+func ensureBuffer(tb testing.TB, buf []byte, n int, want string) {
+	tb.Helper()
 	if got, want := n, len(want); got != want {
-		t.Fatalf("GOT: %v; WANT: %v", got, want)
+		tb.Fatalf("GOT: %v; WANT: %v", got, want)
 	}
 	if got, want := string(buf[:n]), want; got != want {
-		t.Errorf("GOT: %v; WANT: %v", got, want)
+		tb.Errorf("GOT: %v; WANT: %v", got, want)
 	}
 }
 
-func ensureError(t *testing.T, err error, contains string) {
-	t.Helper()
+func ensureError(tb testing.TB, err error, contains string) {
+	tb.Helper()
 	if contains == "" {
 		if err != nil {
-			t.Errorf("GOT: %v; WANT: %v", err, contains)
+			tb.Errorf("GOT: %v; WANT: %v", err, contains)
 		}
 	} else {
 		if err == nil || !strings.Contains(err.Error(), contains) {
-			t.Errorf("GOT: %v; WANT: %v", err, contains)
+			tb.Errorf("GOT: %v; WANT: %v", err, contains)
 		}
 	}
 }
 
-func ensurePanic(t *testing.T, want string, callback func()) {
-	t.Helper()
+func ensurePanic(tb testing.TB, want string, callback func()) {
+	tb.Helper()
 	defer func() {
 		r := recover()
 		if r == nil {
-			t.Fatalf("GOT: %v; WANT: %v", r, want)
+			tb.Fatalf("GOT: %v; WANT: %v", r, want)
 			return
 		}
 		if got := fmt.Sprintf("%v", r); got != want {
-			t.Fatalf("GOT: %v; WANT: %v", got, want)
+			tb.Fatalf("GOT: %v; WANT: %v", got, want)
 		}
 	}()
 	callback()
