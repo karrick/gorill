7b90649b677fde5e6c7bf6121db7cceeb7009136
Karrick S. McDermott <karrick@karrick.net>
2019-05-07 19:17:05 +0000

    better ensure_test.go helpers

--- ensure_test.go
+++ ensure_test.go
@@ -2,6 +2,7 @@ package gorill
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 	"testing"
 )
@@ -16,15 +17,19 @@ func ensureBuffer(tb testing.TB, buf []byte, n int, want string) {
 	}
 }
 
-func ensureError(tb testing.TB, err error, contains string) {
+func ensureError(tb testing.TB, err error, contains ...string) {
 	tb.Helper()
-	if contains == "" {
+	if len(contains) == 0 || (len(contains) == 1 && contains[0] == "") {
 		if err != nil {
 			tb.Errorf("GOT: %v; WANT: %v", err, contains)
 		}
+	} else if err == nil {
+		tb.Errorf("GOT: %v; WANT: %v", err, contains)
 	} else {
-		if err == nil || !strings.Contains(err.Error(), contains) {
-			tb.Errorf("GOT: %v; WANT: %v", err, contains)
+		for _, stub := range contains {
+			if stub != "" && !strings.Contains(err.Error(), stub) {
+				tb.Errorf("GOT: %v; WANT: %q", err, stub)
+			}
 		}
 	}
 }
@@ -42,4 +47,52 @@ func ensurePanic(tb testing.TB, want string, callback func()) {
 		}
 	}()
 	callback()
+}
+
+// ensureNoPanic prettifies the output so one knows which test case caused a
+// panic.
+func ensureNoPanic(tb testing.TB, label string, callback func()) {
+	tb.Helper()
+	defer func() {
+		if r := recover(); r != nil {
+			tb.Fatalf("TEST: %s: GOT: %v", label, r)
+		}
+	}()
+	callback()
+}
+
+func ensureStringSlicesMatch(tb testing.TB, actual, expected []string) {
+	tb.Helper()
+
+	results := make(map[string]int)
+
+	for _, s := range actual {
+		results[s] = -1
+	}
+	for _, s := range expected {
+		results[s] += 1
+	}
+
+	keys := make([]string, 0, len(results))
+	for k := range results {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, s := range keys {
+		v, ok := results[s]
+		if !ok {
+			panic(fmt.Errorf("cannot find key: %s", s)) // panic because this function is broken
+		}
+		switch v {
+		case -1:
+			tb.Errorf("GOT: %q (extra)", s)
+		case 0:
+			// both slices have this key
+		case 1:
+			tb.Errorf("WANT: %q (missing)", s)
+		default:
+			panic(fmt.Errorf("key has invalid value: %s: %d", s, v)) // panic because this function is broken
+		}
+	}
 }
