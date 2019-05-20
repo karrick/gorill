062bb305e1c21542ae6ae88b083144127d6ef0c1
Karrick S. McDermott <karrick@karrick.net>
2019-05-20 18:48:49 +0000

    NewlineCounter returns number of lines read from io.Reader
    Returns the same number of lines read regardless of whether the final
    Read terminated in a newline character.

--- nlc_test.go
+++ nlc_test.go
@@ -5,31 +5,31 @@ import (
 	"testing"
 )
 
-func TestCountLinesFromReader(t *testing.T) {
+func TestNewlineCounter(t *testing.T) {
 	t.Run("sans newline", func(t *testing.T) {
 		t.Run("empty", func(t *testing.T) {
-			c, err := CountLinesFromReader(strings.NewReader(""))
+			c, err := NewlineCounter(strings.NewReader(""))
 			ensureError(t, err)
 			if got, want := c, 0; got != want {
 				t.Errorf("GOT: %v; WANT: %v", got, want)
 			}
 		})
 		t.Run("one", func(t *testing.T) {
-			c, err := CountLinesFromReader(strings.NewReader("one"))
+			c, err := NewlineCounter(strings.NewReader("one"))
 			ensureError(t, err)
 			if got, want := c, 1; got != want {
 				t.Errorf("GOT: %v; WANT: %v", got, want)
 			}
 		})
 		t.Run("two", func(t *testing.T) {
-			c, err := CountLinesFromReader(strings.NewReader("one\ntwo"))
+			c, err := NewlineCounter(strings.NewReader("one\ntwo"))
 			ensureError(t, err)
 			if got, want := c, 2; got != want {
 				t.Errorf("GOT: %v; WANT: %v", got, want)
 			}
 		})
 		t.Run("three", func(t *testing.T) {
-			c, err := CountLinesFromReader(strings.NewReader("one\ntwo\nthree"))
+			c, err := NewlineCounter(strings.NewReader("one\ntwo\nthree"))
 			ensureError(t, err)
 			if got, want := c, 3; got != want {
 				t.Errorf("GOT: %v; WANT: %v", got, want)
@@ -39,28 +39,28 @@ func TestCountLinesFromReader(t *testing.T) {
 
 	t.Run("with newline", func(t *testing.T) {
 		t.Run("empty", func(t *testing.T) {
-			c, err := CountLinesFromReader(strings.NewReader("\n"))
+			c, err := NewlineCounter(strings.NewReader("\n"))
 			ensureError(t, err)
 			if got, want := c, 0; got != want {
 				t.Errorf("GOT: %v; WANT: %v", got, want)
 			}
 		})
 		t.Run("one", func(t *testing.T) {
-			c, err := CountLinesFromReader(strings.NewReader("one\n"))
+			c, err := NewlineCounter(strings.NewReader("one\n"))
 			ensureError(t, err)
 			if got, want := c, 1; got != want {
 				t.Errorf("GOT: %v; WANT: %v", got, want)
 			}
 		})
 		t.Run("two", func(t *testing.T) {
-			c, err := CountLinesFromReader(strings.NewReader("one\ntwo\n"))
+			c, err := NewlineCounter(strings.NewReader("one\ntwo\n"))
 			ensureError(t, err)
 			if got, want := c, 2; got != want {
 				t.Errorf("GOT: %v; WANT: %v", got, want)
 			}
 		})
 		t.Run("three", func(t *testing.T) {
-			c, err := CountLinesFromReader(strings.NewReader("one\ntwo\nthree\n"))
+			c, err := NewlineCounter(strings.NewReader("one\ntwo\nthree\n"))
 			ensureError(t, err)
 			if got, want := c, 3; got != want {
 				t.Errorf("GOT: %v; WANT: %v", got, want)
